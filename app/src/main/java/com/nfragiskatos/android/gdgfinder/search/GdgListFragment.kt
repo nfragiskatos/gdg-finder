3ffd4e0c7721036a8bc7011d048486c4c88c68a0
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2020-05-28 15:33:53 -0700

    - Added Chips to add screen to sort by region.

--- app/src/main/java/com/nfragiskatos/android/gdgfinder/search/GdgListFragment.kt
+++ app/src/main/java/com/nfragiskatos/android/gdgfinder/search/GdgListFragment.kt
@@ -13,7 +13,9 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.nfragiskatos.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
+import com.nfragiskatos.android.gdgfinder.R
 
 private const val LOCATION_PERMISSION_REQUEST = 1
 
@@ -55,6 +57,31 @@ class GdgListFragment : Fragment() {
                     ).show()
                 }
             }
+        })
+
+        viewModel.regionList.observe(viewLifecycleOwner, object: Observer<List<String>> {
+            override fun onChanged(data: List<String>?) {
+                data ?: return
+                val chipGroup = binding.regionList
+                val inflator = LayoutInflater.from(chipGroup.context)
+
+                val children = data.map { regionName ->
+                    val chip = inflator.inflate(R.layout.region, chipGroup, false) as Chip
+
+                    chip.text = regionName
+                    chip.tag = regionName
+                    chip.setOnCheckedChangeListener{button, isChecked ->
+                        viewModel.onFilterChanged(button.tag as String, isChecked)
+                    }
+                    chip
+                }
+                chipGroup.removeAllViews()
+
+                for (chip in children) {
+                    chipGroup.addView(chip)
+                }
+            }
+
         })
 
         setHasOptionsMenu(true)
